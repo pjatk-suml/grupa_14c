caa2972a726be92ff1a515e8ce44fafe7b9526bb
LukaszMalinowski <lukasz.malinowski10@gmail.com>
2021-12-27 20:13:47 +0100

    Added taking picture with camera on frontend

---
+++ static/js/index.js
@@ -0,0 +1,36 @@
+let video = document.querySelector("#videoElement");
+let screenshot = document.querySelector("#screenshot");
+let image = document.querySelector("#image");
+let countdown = document.querySelector("#countdown");
+
+const takeScreenshot = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    let count = 3;
+    const timer = setInterval(() => {
+        if (count === 0) {
+            clearInterval(timer);
+
+            countdown.innerHTML = "";
+            canvas.getContext('2d')
+                .drawImage(video, 0, 0, canvas.width, canvas.height);
+            const dataUrl = canvas.toDataURL();
+            image.src = dataUrl;
+        } else {
+            countdown.innerHTML = count;
+            count--;
+        }
+    }, 1000);
+}
+
+screenshot.addEventListener("click", takeScreenshot);
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(stream => video.srcObject = stream)
+        .catch(error => console.log(error));
+} else {
+    alert("You need webcam to use this application");
+}
